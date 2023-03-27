7f9f649f4faee3e5e37cc05c172cad6bbbdabf6f
Brad Bishop <bradleyb@fuzziesquirrel.com>
2023-03-26 21:23:51 -0400

    ui: remove incline
    This looks nice but I don't have time to figure out how to configure it
    right now, and the defaults don't work for me.

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -1,37 +1,7 @@
 return {
     {
         "akinsho/bufferline.nvim",
-        opts = { options = { separator_style = "slant", mode = "tabs" } },
-    },
-    {
-        "b0o/incline.nvim",
-        event = "BufReadPre",
-        config = function()
-            require("incline").setup({
-                highlight = {
-                    groups = {
-                        InclineNormal = {
-                            guibg = "#FC56B1",
-                            -- guifg = colors.black,
-                        },
-                        InclineNormalNC = {
-                            guifg = "#FC56B1",
-                            -- guibg = colors.black,
-                        },
-                    },
-                },
-                window = { margin = { vertical = 0, horizontal = 1 } },
-                render = function(props)
-                    local filename = vim.fn.fnamemodify(
-                        vim.api.nvim_buf_get_name(props.buf),
-                        ":t"
-                    )
-                    local icon, color =
-                        require("nvim-web-devicons").get_icon_color(filename)
-                    return { { icon, guifg = color }, { " " }, { filename } }
-                end,
-            })
-        end,
+        opts = { options = { separator_style = "slant" } },
     },
     {
         "iamcco/markdown-preview.nvim",
