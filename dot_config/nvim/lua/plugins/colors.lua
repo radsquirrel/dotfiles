ee824090b843cc282febdafb95a200804115351c
Brad Bishop <bradleyb@fuzziesquirrel.com>
2023-03-28 21:32:39 -0400

    nvim: gruvbox: tweak SignColumn
    I like this to match the main buffer background.

--- dot_config/nvim/lua/plugins/colors.lua
+++ dot_config/nvim/lua/plugins/colors.lua
@@ -1,5 +1,35 @@
 return {
-    { "ellisonleao/gruvbox.nvim" },
+    {
+        "ellisonleao/gruvbox.nvim",
+        opts = {
+            overrides = {
+                SignColumn = {
+                    bg = "#282828",
+                },
+                GruvboxGreenSign = {
+                    bg = "#282828",
+                },
+                GruvboxAquaSign = {
+                    bg = "#282828",
+                },
+                GruvboxBlueSign = {
+                    bg = "#282828",
+                },
+                GruvboxYellowSign = {
+                    bg = "#282828",
+                },
+                GruvboxRedSign = {
+                    bg = "#282828",
+                },
+                GruvboxPurpleSign = {
+                    bg = "#282828",
+                },
+                GruvboxOrangeSign = {
+                    bg = "#282828",
+                },
+            },
+        },
+    },
     {
         "LazyVim/LazyVim",
         opts = {
