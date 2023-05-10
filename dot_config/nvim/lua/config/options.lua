00779a96da2a4124f1c00949f3048da81185678b
Brad Bishop <bradleyb@fuzziesquirrel.com>
2023-05-09 20:21:32 -0400

    nvim: trying nvim-ufo

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,16 @@
 vim.opt.number = false
 vim.opt.tabstop = 8
 vim.opt.relativenumber = false
+
+vim.opt.fillchars = {
+    foldopen = "",
+    foldclose = "",
+    fold = " ",
+    foldsep = " ",
+    diff = "╱",
+    eob = " ",
+}
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+vim.o.foldcolumn = "0"
