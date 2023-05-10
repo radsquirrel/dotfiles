8c829e757e43feb9f95bdd0bd8d3d3fae0117887
Brad Bishop <bradleyb@fuzziesquirrel.com>
2023-05-09 20:14:04 -0400

    nvim: add floating winbar plugin

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -1,4 +1,21 @@
 return {
+    -- floating winbar
+    {
+        "b0o/incline.nvim",
+        event = "BufReadPre",
+        opts = {
+            window = { margin = { vertical = 0, horizontal = 1 } },
+            render = function(props)
+                local filename = vim.fn.fnamemodify(
+                    vim.api.nvim_buf_get_name(props.buf),
+                    ":t"
+                )
+                local icon, color =
+                    require("nvim-web-devicons").get_icon_color(filename)
+                return { { icon, guifg = color }, { " " }, { filename } }
+            end,
+        },
+    },
     {
         "nvim-neo-tree/neo-tree.nvim",
         dependencies = "s1n7ax/nvim-window-picker",
