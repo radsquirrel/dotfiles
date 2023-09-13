41826f5c9038d59905aad585a61410eb34a67467
Brad Bishop <bradleyb@fuzziesquirrel.com>
2023-09-13 15:23:21 -0400

    fish: use a more natural completion keybinding

--- dot_config/fish/functions/fish_user_key_bindings.fish
+++ dot_config/fish/functions/fish_user_key_bindings.fish
@@ -2,5 +2,6 @@ function fish_user_key_bindings
     fish_vi_key_bindings
     bind --mode insert --sets-mode default jk backward-char force-repaint
     bind --mode visual --sets-mode default jk backward-char force-repaint
-    bind --mode default \\s accept-autosuggestion
+    bind --mode default \cy accept-autosuggestion
+    bind --mode insert \cy accept-autosuggestion
 end
